7ace91402adf974430240257f7a1244a657f8a6d
Piotr Klibert <piotr.klibert@10clouds.com>
2017-07-10 22:01:06 +0200

    Slowly moving to asdf-defined configuration

--- init.lisp
+++ init.lisp
@@ -1,8 +1,19 @@
+(asdf:initialize-source-registry
+ '(:source-registry
+   (:directory "/home/cji/.stumpwm.d/config")
+   :inherit-configuration))
+
 (ql:quickload "cl-arrows")
 (ql:quickload "split-sequence")
 (ql:quickload "xembed")
 (ql:quickload "alexandria")
 (ql:quickload "clx-truetype")
+(ql:quickload "lparallel")
+(ql:quickload "optima")
+
+(use-package '(:optima :lparallel :alexandria :cl-arrows))
+
+(setf lparallel:*kernel* (lparallel:make-kernel 8))
 
 (set-module-dir "/home/cji/portless/stumpwm/contrib")
 (load-module "ttf-fonts")
@@ -19,13 +30,18 @@
                                        :direction :output
                                        :if-exists :append
                                        :if-does-not-exist :create))
+;; Enable SLIME/Emacs integration
+(load "/home/cji/.stumpwm.d/swank.lisp")
+
+(asdf:load-system "stumpwm-config")
 
 (setf *startup-message* (format nil "Welcome to ~a!" (machine-instance)))
 (setf *window-format* "%50t")
 
+(use-package :my-utils)
 (load "/home/cji/.stumpwm.d/utils.lisp")  ; TODO: refactor!
 
-(shell$
+(shell$ :wait t
  ; generate necessary font files if needed
  "sudo mkfontscale /usr/share/fonts/bitstream-vera/"
  "sudo mkfontdir /usr/share/fonts/bitstream-vera/"
@@ -46,7 +62,12 @@
                            :subfamily "Roman" :size 14
                            :antialiased t)))
 
-(add-hook *start-hook* #'(lambda () (shell$ "copyq")))
+(add-hook *start-hook*
+          #'(lambda ()
+              ;; (shell$ "skypeforlinux" "copyq" "emacs" "google-chrome" "urxvt")
+              ))
+
+
 ;; Create the three usual workspaces.
 (add-hook *start-hook* #'(lambda () (run-commands "grename CHROME"
                                                   "gnewbg EMACS"
@@ -66,6 +87,3 @@
 
 ;; Modeline widgets definitions
 (load "/home/cji/.stumpwm.d/modeline.lisp")
-
-;; Enable SLIME/Emacs integration
-(load "/home/cji/.stumpwm.d/swank.lisp")
